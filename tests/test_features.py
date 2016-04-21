7a017e500169934de11362219d2d390c39466692
Ashutosh Dwivedi <adwivedi@akamai.com>
2016-04-21 19:53:28 +0530

    added barebones project

---
+++ tests/test_features.py
@@ -0,0 +1,17 @@
+import numpy as np
+import unittest
+
+from voilajones import integral_image
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+    def test_integral_image(self):
+        np.ones()
+        integral_image.get_integral_image()
---
+++ voilajones/integral_image.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def get_integral_image(image):
+    return []
