ca0b4d1a30db7ddb6c3d8555d401b5fb3feed515
Ashutosh Dwivedi <adwivedi@akamai.com>
2016-04-23 10:55:08 +0530

    added area sum

--- tests/test_features.py
+++ tests/test_features.py
@@ -23,3 +23,23 @@ class Test(unittest.TestCase):
         returned = vj.features.get_integral_image(ones)
 
         assert (returned == expected).all(), returned
+
+    def test_area_sum(self):
+        ones = np.ones((3, 3))
+        integral_image = vj.features.get_integral_image(ones)
+
+        top_left = (0, 0)
+        height = 1
+        width = 1
+        returned = vj.features.get_area_sum(integral_image, top_left,
+                                            height, width)
+        assert returned == 1.0
+
+        height = 2
+        width = 2
+        returned = vj.features.get_area_sum(integral_image, top_left,
+                                    height, width)
+        assert returned == 4.0
+
+
+
--- voilajones/features.py
+++ voilajones/features.py
@@ -1,4 +1,33 @@
 import numpy as np
+from collections import namedtuple
+
+
+class Feature(object):
+    def __init__(self, integral_image, top_left, bottom_right):
+        """
+        :param integral_image: summed area table of the image
+        :param top_left: coordinates of the top left point
+        :param bottom_right: coordinates of the bottom right point
+        """
+        self.integral_image = integral_image
+
+    def get_score(self):
+        raise NotImplementedError('Not implemented use a subclass')
+
+    @property
+    def type(self):
+        raise NotImplemented('Not implemented use a subclass')
+
+
+class FEATURE_TYPES:
+    TWO_VERTICAL = ''
+
+
+class TwoRectangleVertical(Feature):
+    type = FEATURE_TYPES.TWO_VERTICAL
+
+    def get_score(self):
+        return self.top_left - self.bottom_right
 
 
 def get_integral_image(image):
@@ -19,3 +48,25 @@ def get_integral_image(image):
             if i - 1 >= 0 and j - 1 >= 0:
                 result[i][j] -= result[i-1][j-1]
     return result
+
+
+def get_area_sum(integral_image, top_left, height, width):
+    top_right = (top_left[0], top_left[1] + width)
+    bottom_right = (top_right[0] + height, top_right[1])
+    bottom_left = (top_left[0] + height, top_left[1])
+
+    return (integral_image[top_left] + integral_image[bottom_right] -
+            integral_image[top_right] - integral_image[bottom_left])
+
+
+
+def get_feature(feature_type, x, y, width, height):
+    """
+
+    :param feature_type:
+    :param x:
+    :param y:
+    :param width:
+    :param height:
+    :return:
+    """
